d74587637b4c9c5ea64e767fc3826f0cc833505b
wei-x15 <wei-x15@mails.tsinghua.edu.cn>
2019-02-28 13:45:39 +0800

    add output drawing utilities

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include <opencv2/opencv.hpp>
 
 #include "Detector.h"
+#include "util.h"
 
 using namespace std;
 using namespace std::chrono;
 
 int main(int argc, const char *argv[]) {
+    auto classes = load_classes("data/coco.names");
+    auto cmap = color_map(classes.size());
+
     if (argc != 2) {
         std::cerr << "usage: yolo-app <image path>" << std::endl;
         return -1;
@@ -27,26 +31,12 @@ int main(int argc, const char *argv[]) {
 
     while (cap.read(origin_image)) {
         auto start = std::chrono::high_resolution_clock::now();
-
-        auto result = detector.detect(origin_image);
-
+        auto detections = detector.detect(origin_image);
         auto end = std::chrono::high_resolution_clock::now();
         std::cout << "FPS: " << 1000 / duration_cast<milliseconds>(end - start).count() << endl;
 
-        auto bbox = std::get<0>(result);
-        auto classes = std::get<1>(result);
-        auto scores = std::get<2>(result);
-
-        std::cout << bbox.size(0) << " objects found" << endl;
-
-        auto result_data = bbox.accessor<float, 2>();
-
-        for (int i = 0; i < bbox.size(0); i++) {
-            cv::rectangle(origin_image, cv::Point(result_data[i][0], result_data[i][1]),
-                          cv::Point(result_data[i][2], result_data[i][3]), cv::Scalar(0, 0, 255), 1, 1, 0);
-        }
+        draw_detections(origin_image, detections, classes, cmap);
         cv::imshow("out", origin_image);
-
         if (cv::waitKey(1) != -1) break;
     }
 
---
+++ util.h
@@ -0,0 +1,96 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <torch/torch.h>
+#include <sstream>
+
+#include "bbox.h"
+
+using ClassList=std::vector<std::string>;
+
+inline ClassList load_classes(const std::string &path) {
+    ClassList out;
+    std::ifstream fp(path);
+    while (fp) {
+        std::string cls;
+        std::getline(fp, cls);
+        if (!cls.empty()) {
+            out.push_back(cls);
+        }
+    }
+    fp.close();
+    return out;
+}
+
+using ColorMap=std::vector<cv::Scalar>;
+
+inline ColorMap color_map(int64_t n) {
+    auto bit_get = [](int64_t x, int64_t i) { return x & (1 << i); };
+    ColorMap cmap;
+
+    for (int64_t i = 0; i < n; ++i) {
+        int64_t r = 0, g = 0, b = 0;
+        int64_t i_ = i;
+        for (int64_t j = 7; j >= 0; --j) {
+            r |= bit_get(i_, 0) << j;
+            g |= bit_get(i_, 1) << j;
+            b |= bit_get(i_, 2) << j;
+            i_ >>= 3;
+        }
+        cmap.emplace_back(b, g, r);
+    }
+    return cmap;
+}
+
+inline void draw_text(cv::Mat &img, const std::string &str,
+                      const cv::Scalar &color, cv::Point pos, bool reverse = false) {
+    auto t_size = cv::getTextSize(str, cv::FONT_HERSHEY_PLAIN, 1, 1, nullptr);
+    cv::Point bottom_left, upper_right;
+    if (reverse) {
+        upper_right = pos;
+        bottom_left = cv::Point(upper_right.x - t_size.width, upper_right.y + t_size.height);
+    } else {
+        bottom_left = pos;
+        upper_right = cv::Point(bottom_left.x + t_size.width, bottom_left.y - t_size.height);
+    }
+
+    cv::rectangle(img, bottom_left, upper_right, color, -1);
+    cv::putText(img, str, bottom_left, cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(255, 255, 255, 255) - color);
+}
+
+template<typename TLabelFn, typename TColorFn>
+inline void draw_bbox(cv::Mat &img, torch::Tensor bbox, TLabelFn label_fn, TColorFn color_fn) {
+    auto bbox_acc = bbox.accessor<float, 2>();
+    for (int64_t i = 0; i < bbox_acc.size(0); ++i) {
+        auto p1 = cv::Point(bbox_acc[i][0], bbox_acc[i][1]);
+        auto p2 = cv::Point(bbox_acc[i][2], bbox_acc[i][3]);
+        cv::rectangle(img, p1, p2, color_fn(i));
+
+        auto label = label_fn(i);
+        if (!label.empty()) {
+            draw_text(img, label, color_fn(i), p1);
+        }
+    }
+}
+
+inline void draw_detections(cv::Mat &img, const Detection &detections,
+                            const ClassList &classes, const ColorMap &cmap) {
+    auto&[bbox, cls, scr] = detections;
+    auto cls_acc = cls.accessor<int64_t, 1>();
+    auto scr_acc = scr.accessor<float, 1>();
+    std::ostringstream str;
+
+    auto label_fn = [&](int64_t i) {
+        str.str("");
+        str << classes[cls_acc[i]] << " "
+            << std::fixed << std::setprecision(2) << scr_acc[i];
+        return str.str();
+    };
+    auto color_fn = [&](int64_t i) {
+        return cmap[cls_acc[i]];
+    };
+
+    draw_bbox(img, bbox, label_fn, color_fn);
+}
+
