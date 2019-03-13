bb8100fb9f278fc88f804d32e8c4de9ed953b4e8
wei-x15 <wei-x15@mails.tsinghua.edu.cn>
2019-03-13 14:55:45 +0800

    split Target.cpp

---
+++ demo/Target.cpp
@@ -0,0 +1,51 @@
+#include "Target.h"
+
+
+void TargetRepo::update(const std::vector<Track> &trks, int frame, const cv::Mat &image) {
+    std::map<int, int> trk_tgt_map;
+    for (int i = 0; i < targets.size(); ++i) {
+        for (auto j:targets[i].second) {
+            assert(trk_tgt_map.count(j) == 0);
+            trk_tgt_map[j] = i;
+        }
+    }
+    for (auto i:discard_trks) {
+        assert(trk_tgt_map.count(i) == 0);
+        trk_tgt_map[i] = -1;
+    }
+
+    for (auto &[id, box]:trks) {
+        if (!trk_tgt_map.count(id)) { // new track is target
+            TargetWrap wrap;
+            wrap.first = Target(std::make_pair(frame, box),
+                                image(unnormalize_rect(pad_rect(box, padding), image.cols,
+                                                       image.rows)).clone());
+            wrap.second = {id};
+            targets.push_back(std::move(wrap));
+        } else if (trk_tgt_map[id] != -1) { // add track to target
+            targets[trk_tgt_map[id]].first.trajectories.emplace(frame, box);
+        }
+    }
+}
+
+void TargetRepo::merge(TargetRepo::size_type to, TargetRepo::size_type from) {
+    auto &[target_to, trks_to] = targets[to];
+    auto &[target_from, trks_from] = targets[from];
+
+    // merge trajectories
+    // the target merge from has higher priority
+    target_from.trajectories.merge(target_to.trajectories);
+    target_to.trajectories = std::move(target_from.trajectories);
+
+    // merged target should have all tracks
+    trks_to.insert(trks_to.end(), trks_from.begin(), trks_from.end());
+
+    targets.erase(targets.begin() + from);
+}
+
+void TargetRepo::erase(TargetRepo::size_type idx) {
+    for (auto i:targets[idx].second) {
+        discard_trks.push_back(i);
+    }
+    targets.erase(targets.begin() + idx); // delete the target
+}
--- demo/Target.h
+++ demo/Target.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <utility>
 #include <opencv2/opencv.hpp>
+#include <GL/gl3w.h>
 
 #include "util.h"
 
@@ -54,59 +55,14 @@ public:
 
     size_type size() { return targets.size(); }
 
-    const Target &operator[](size_type idx) {
-        return targets[idx].first;
-    }
+    const Target &operator[](size_type idx) { return targets[idx].first; }
 
     void update(const std::vector<Track> &trks,
-                int frame, const cv::Mat &image) {
-        std::map<int, int> trk_tgt_map;
-        for (int i = 0; i < targets.size(); ++i) {
-            for (auto j:targets[i].second) {
-                assert(trk_tgt_map.count(j) == 0);
-                trk_tgt_map[j] = i;
-            }
-        }
-        for (auto i:discard_trks) {
-            assert(trk_tgt_map.count(i) == 0);
-            trk_tgt_map[i] = -1;
-        }
+                int frame, const cv::Mat &image);
 
-        for (auto &[id, box]:trks) {
-            if (!trk_tgt_map.count(id)) { // new track is target
-                TargetWrap wrap;
-                wrap.first = Target(std::make_pair(frame, box),
-                                    image(unnormalize_rect(pad_rect(box, padding), image.cols,
-                                                           image.rows)).clone());
-                wrap.second = {id};
-                targets.push_back(std::move(wrap));
-            } else if (trk_tgt_map[id] != -1) { // add track to target
-                targets[trk_tgt_map[id]].first.trajectories.emplace(frame, box);
-            }
-        }
-    }
+    void merge(size_type to, size_type from);
 
-    void merge(size_type to, size_type from) {
-        auto &[target_to, trks_to] = targets[to];
-        auto &[target_from, trks_from] = targets[from];
-
-        // merge trajectories
-        // the target merge from has higher priority
-        target_from.trajectories.merge(target_to.trajectories);
-        target_to.trajectories = std::move(target_from.trajectories);
-
-        // merged target should have all tracks
-        trks_to.insert(trks_to.end(), trks_from.begin(), trks_from.end());
-
-        targets.erase(targets.begin() + from);
-    }
-
-    void erase(size_type idx) {
-        for (auto i:targets[idx].second) {
-            discard_trks.push_back(i);
-        }
-        targets.erase(targets.begin() + idx); // delete the target
-    }
+    void erase(size_type idx);
 
 private:
     static constexpr float padding = 0.1f;
